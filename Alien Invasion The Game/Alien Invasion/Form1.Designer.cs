5bd551804f89ce1835dc13f83c20a00394d2e233
xMattAdams <mat.adamczyk@icloud.com>
2020-03-12 20:40:53 +0100

    Alien Invasion The Game

---
+++ Alien Invasion The Game/Alien Invasion/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Alien_Invasion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.ufo = new System.Windows.Forms.PictureBox();
+            this.pillar2 = new System.Windows.Forms.PictureBox();
+            this.pillar1 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ufo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "00";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gametick);
+            // 
+            // ufo
+            // 
+            this.ufo.Image = global::Alien_Invasion.Properties.Resources.alien1;
+            this.ufo.Location = new System.Drawing.Point(608, 149);
+            this.ufo.Name = "ufo";
+            this.ufo.Size = new System.Drawing.Size(68, 72);
+            this.ufo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.ufo.TabIndex = 3;
+            this.ufo.TabStop = false;
+            // 
+            // pillar2
+            // 
+            this.pillar2.Image = global::Alien_Invasion.Properties.Resources.pillar;
+            this.pillar2.Location = new System.Drawing.Point(495, 296);
+            this.pillar2.Name = "pillar2";
+            this.pillar2.Size = new System.Drawing.Size(56, 146);
+            this.pillar2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pillar2.TabIndex = 2;
+            this.pillar2.TabStop = false;
+            // 
+            // pillar1
+            // 
+            this.pillar1.Image = global::Alien_Invasion.Properties.Resources.pillar;
+            this.pillar1.Location = new System.Drawing.Point(338, -6);
+            this.pillar1.Name = "pillar1";
+            this.pillar1.Size = new System.Drawing.Size(56, 150);
+            this.pillar1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pillar1.TabIndex = 1;
+            this.pillar1.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.Image = global::Alien_Invasion.Properties.Resources.Halicopter;
+            this.player.Location = new System.Drawing.Point(87, 179);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(100, 54);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(795, 364);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ufo);
+            this.Controls.Add(this.pillar2);
+            this.Controls.Add(this.pillar1);
+            this.Controls.Add(this.player);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Alien Invasion ";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.ufo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox pillar1;
+        private System.Windows.Forms.PictureBox pillar2;
+        private System.Windows.Forms.PictureBox ufo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ Alien Invasion The Game/Alien Invasion/Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alien_Invasion
+{
+    public partial class Form1 : Form
+    {
+
+        //Wartosci
+        bool goup; //mozemy leciec w gore
+        bool godown; //mozemy leciec w dol
+        bool shot = false; //sprawdzenie czy wystrzelilismy jakies pociski
+        int score = 0; //licznik punktow
+        int speed = 8; //szybkosc ufo i czasteczek
+        Random rand = new Random(); //losowosc w generowaniu
+        int playerSpeed = 7; //szybkosc helikoptera
+        int index; //to do zmieniania grafik ufokow
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                //wciskamy przycisk - lecimy w gore
+                goup = true; 
+            
+
+            }
+
+            if (e.KeyCode==Keys.Down)
+            {
+                //wciskamy przycisk - lecimy w dol
+                godown = true;
+            }
+              
+               //kiedy wciskamy spacje,ale nie strzelalismy przed jej wcisnieciem - strzelamy tylko,gdy mamy wcisnieta spacje
+            if(e.KeyCode == Keys.Space && shot == false)
+            {
+                makeBullet();
+                shot = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Up)
+            {
+                //puszczamy przycisk - nie lecimy w gore
+                goup = false;
+            }
+
+            if(e.KeyCode == Keys.Up)
+            {
+                //puszczamy przycisk - nie lecimy w dol
+                godown = false;
+            }
+             
+            if(shot == true)
+            {
+                //puszczamy spacje - nie strzelamy
+                shot = false;
+
+            }
+        }
+
+        private void gametick(object sender, EventArgs e)
+        {
+            //pillar1 leci w lewa strone
+            pillar1.Left -= speed;
+
+            //pillar2 leci w lewa strone
+            pillar2.Left -= speed;
+
+            //ufo leci w lewa strone
+
+            ufo.Left -= speed;
+
+            foreach (Control X in this.Controls)
+            {
+                //jeśli X jest pictureboxem i (AND) ma nazwe "bullet",to wtedy zachowa się,według poniższych komend
+                if (X is PictureBox && (string)X.Tag == "bullet")
+                {
+
+                    //pocisk leci w prawo
+                    X.Left += 15;
+
+                    //jesli pocisk wyleci poza ekran z prawej,to zniknie
+                    if (X.Left > 900)
+                    {
+                        this.Controls.Remove(X);
+
+                        //usuwamy X z pamieci aplikacji
+                        X.Dispose();
+                    }
+
+                    //pocisk zderza sie z UFO,znika i dostajemy pkt,a UFO jest przeniesione poza screen - znika
+                    if (X.Bounds.IntersectsWith(ufo.Bounds))
+                    {
+                        score += 1;
+
+                        this.Controls.Remove(X);
+
+                        X.Dispose();
+
+                        ufo.Left = 1000;
+
+
+                        //UFO pojawia sie w randomowej lokacji w pionie na screenie
+                        ufo.Top = rand.Next(40, 330) - ufo.Height;
+
+                        //uruchomienie funkcji,ktora zmienia image UFO
+                        changeUFO();
+                    }
+
+                }
+
+
+            }
+
+            //wynik pokaze sie w labelu
+            label1.Text = "Score:" + score;
+
+            if (goup)
+            {
+                player.Top -= playerSpeed;
+            }
+
+            if (godown)
+            {
+                player.Top += playerSpeed;
+            }
+             
+            //jesli pillar wyleci poza screena,pojawi sie znowu z przodu
+            if(pillar1.Left < -150)
+            {
+                pillar1.Left = 900;
+            }
+
+            if (pillar2.Left < -150)
+            {
+                pillar2.Left = 1000;
+            }
+
+             //jesli ufo wylecialo poza screen albo gracz zderzyl sie z ufo albo z pillarami,to koniec gry 
+             if(ufo.Left < -5 ||
+                player.Bounds.IntersectsWith(ufo.Bounds) ||
+                player.Bounds.IntersectsWith(pillar1.Bounds) ||
+                player.Bounds.IntersectsWith(pillar2.Bounds) 
+                )
+
+            {
+                //koniec gry
+                gameTimer.Stop();
+
+                //okienko z info o przegranej i ze statami
+                MessageBox.Show("Przegrałeś,zniszczyłeś  " + score + " statków UFO");
+
+                //znikanie pocisku i trafionego UFO,zachowanie pocisku,kolizja pocisku z UFO,punkty za trafione UFO 
+             
+            }
+                                                                   
+
+        }
+
+        private void changeUFO()
+        {
+            index += 1; //najpierw zwiekszamy index,pozniej go porownujemy
+
+            if (index > 3)
+            {
+                //jesli index osiagnie wartosc wieksza niz 3,wraca do wartosci 1,zeby naprzemian losowac 3 skiny ufo
+                index = 1;
+            }
+
+            //zmiana skina ufo
+            switch(index)
+            {
+                //jesli index osiagnie wartosc 1,pojawi sie skin 1 na pictureboxie4
+                case 1:
+                    ufo.Image = Properties.Resources.alien1;
+                    break;
+
+                //jesli index osiagnie wartosc 2,pojawi sie skin 2 na pictureboxie4
+                case 2:
+                    ufo.Image = Properties.Resources.alien2;
+                    break;
+
+                //jesli index osiagnie wartosc 3,pojawi sie skin 3 na pictureboxie4
+                case 3:
+                    ufo.Image = Properties.Resources.alien3;
+                    break;
+            }
+
+
+        }
+        
+        private void makeBullet()
+        {
+            //nowy picturebox dla pocisku
+            PictureBox bullet = new PictureBox();
+
+            //kolorek pocisku
+            bullet.BackColor = System.Drawing.Color.DarkOrange;
+
+            //wysokosc pocisku
+            bullet.Height = 5;
+
+            //szerokosc pocisku
+            bullet.Width = 10;
+
+            //pocisk zrespi sie przed helikopterem
+            bullet.Left = player.Left + player.Width;
+
+            //pocisk zrespi sie przed helikopterem,ale idealnie posrodku
+            bullet.Top = player.Top + player.Height /2;
+
+            
+            //nazywamy pocisk jako ,,bullet"
+            bullet.Tag = "bullet";
+
+            //dodajemy pocisk do gry
+            this.Controls.Add(bullet);
+                
+                
+                
+                
+                
+                
+                
+        }
+
+
+
+
+    }
+}
