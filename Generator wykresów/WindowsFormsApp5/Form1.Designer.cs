63271df1c70a0b1a667f1e2eab81f57c65039ac0
xMattAdams <mat.adamczyk@icloud.com>
2020-03-12 20:27:14 +0100

    Chart generator

---
+++ Generator wykresów/WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1062, 546);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(932, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(130, 546);
+            this.panel1.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 2;
+            this.numericUpDown1.Location = new System.Drawing.Point(7, 12);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Gold;
+            this.button4.Location = new System.Drawing.Point(4, 486);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(126, 57);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Czyść";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Gold;
+            this.button3.Location = new System.Drawing.Point(4, 346);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 57);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Wykres DNA";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gold;
+            this.button2.Location = new System.Drawing.Point(4, 216);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 57);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Wykres cos(x)";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gold;
+            this.button1.Location = new System.Drawing.Point(4, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Układ współrzędnych";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1062, 546);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Generator wykresów/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            //x
+            g.DrawLine(new Pen(new SolidBrush(Color.Red)), new Point(0, 200), new Point(800, 200));
+            //y
+            g.DrawLine(new Pen(new SolidBrush(Color.Red)), new Point(440, 50), new Point(440, 350));
+
+            //strzałka x
+            g.DrawLine(new Pen(new SolidBrush(Color.Red)), new Point(680, 195), new Point(700, 200));
+            g.DrawLine(new Pen(new SolidBrush(Color.Red)), new Point(680, 205), new Point(700, 200));
+
+            //strzałka y
+            g.DrawLine(new Pen(new SolidBrush(Color.Red)), new Point(435, 70), new Point(440, 50));
+            g.DrawLine(new Pen(new SolidBrush(Color.Red)), new Point(445, 70), new Point(440, 50));
+
+            //podziałki na x
+            int x_ = 0;
+            int y_ = 200;
+            for (int x = 0; x<15; x++)
+            {
+                g.DrawLine(new Pen(new SolidBrush(Color.Red)), new Point(x_ + 63, y_ + 3), new Point(x_ + 63, y_ - 3));
+                x_ += 63;
+            }
+
+            //podziałki na y
+            x_ = 440;
+             y_ = 50;
+            for (int x=0; x<6; x++)
+            {
+                g.DrawLine(new Pen(new SolidBrush(Color.Red)), new Point(x_ - 3, y_ + 30), new Point(x_ + 3, y_ + 30));
+                y_ += 40;
+            }
+
+
+
+            
+
+
+        }
+        float xMax = 40;
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            xMax = (float)numericUpDown1.Value; 
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            
+            //cosinusik <3
+            float x1 = 0;
+            float y1 = 0;
+            float y2 = 0;
+            float yMax = 200;
+       
+            Pen p = new Pen(Brushes.Black, 3.0F);
+            for (float x = 0; x < 39; x += 0.2F)
+            {
+                y2 = (float)Math.Sin(x);
+
+                g.DrawLine(p, x1 * xMax, y1 * xMax + yMax, x * xMax, y2 * xMax + yMax);
+
+                x1 = x;
+                y1 = y2;
+            }
+
+            
+        }
+       
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            Graphics g = pictureBox1.CreateGraphics();
+            //sinusik DNA <3
+            float x2 = 0;
+            float y3 = 0;
+            float y4 = 0;
+            float yMax2 = 200;
+            float xMax2 = 46;
+            Pen p2 = new Pen(Brushes.Black, 3.0F);
+            for (float x = 0; x < 30; x += 0.2F)
+            {
+                y3 = (float)Math.Sin(x);
+
+                g.DrawLine(p2, x2 * xMax2, y3 * xMax2 + yMax2, x * xMax2, y4 * xMax2 + yMax2);
+
+                x2 = x;
+                y3 = y4;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Invalidate();
+        }
+
+       
+    }
+}
