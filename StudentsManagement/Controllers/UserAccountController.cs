41c51625013a859833228932a7e4105403781525
Mateusz Adamczyk <mat.adamczyk@icloud.com>
2020-11-04 01:40:08 +0100

    feature-added-registration
    New bug from MySQL connector - conversion from boolean to int. Trying to solve it.

---
+++ StudentsManagement/Controllers/UserAccountController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StudentsManagement.ViewModels;
+
+namespace StudentsManagement.Controllers
+{
+    public class UserAccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+
+        public UserAccountController(UserManager<IdentityUser>userManager, SignInManager<IdentityUser>signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+        
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    
+    [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var newUser = new IdentityUser { UserName = registerModel.Email, Email = registerModel.Email };
+                var response = await userManager.CreateAsync(newUser, registerModel.Password);
+
+                if (response.Succeeded)
+                {
+                    await signInManager.SignInAsync(newUser, isPersistent: false);
+                    return RedirectToAction("index", "home");
+                }
+                 foreach(var error in response.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            
+            return View(registerModel);
+        }
+
+
+    }
+}
---
+++ StudentsManagement/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentsManagement.ViewModels
+{
+    public class RegisterViewModel
+    {
+
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name ="Confirm your password")]
+        [Compare("Password", ErrorMessage ="The confirmed password does not match the actual password")]
+        public string PasswordConfirm { get; set; }
+    }
+}
