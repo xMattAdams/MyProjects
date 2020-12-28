fcd5c5960a6ca33f3106b189fff31b21c9e10ddd
Mateusz Adamczyk <mat.adamczyk@icloud.com>
2020-12-28 05:57:08 +0100

    feature-add-editrole-controller
    .

--- StudentsManagement/Controllers/AdminController.cs
+++ StudentsManagement/Controllers/AdminController.cs
@@ -2,7 +2,9 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.EntityFrameworkCore.Update;
+using StudentsManagement.Models;
 using StudentsManagement.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -15,10 +17,12 @@ namespace StudentsManagement.Controllers
 
     {
         private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<ApplicationUser> userManager;
 
-        public AdminController(RoleManager<IdentityRole> roleManager)
+        public AdminController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
         {
             this.roleManager = roleManager;
+            this.userManager = userManager;
         }
 
         [Authorize][HttpGet]
@@ -61,6 +65,64 @@ namespace StudentsManagement.Controllers
         {
             var roles = roleManager.Roles;
             return View(roles);
+        }
+
+   [HttpGet]
+    public async Task<IActionResult>EditRole(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Couldn't find the role with Id = {id}";
+                return View("ErrorNotFound");
+            }
+
+            var model = new EditRoleViewModel
+            {
+                RoleId = role.Id,
+                RoleName = role.Name
+            };
+       
+        
+        foreach(var user in userManager.Users)
+            {
+              if (await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+            return View(model);
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            var role = await roleManager.FindByIdAsync(model.RoleId);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Couldn't find the role with Id = {model.RoleId}";
+                return View("ErrorNotFound");
+            }
+            else
+            {
+                role.Name = model.RoleName;
+              var updateResult = await roleManager.UpdateAsync(role);
+
+                if (updateResult.Succeeded)
+                {
+                    return RedirectToAction("GetRoles");
+                }
+                 foreach(var error in updateResult.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View(model);
+            }
+            
         }
 
     }
---
+++ StudentsManagement/ViewModels/EditRoleViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentsManagement.ViewModels
+{
+    public class EditRoleViewModel
+    {
+
+        public EditRoleViewModel()
+        {
+            Users = new List<string>();
+        }
+
+
+        public string RoleId { get; set; }
+        
+        [Required(ErrorMessage = "You have to add the role name!")]
+        public string RoleName { get; set; }
+
+        public List<string> Users { get; set; }
+    }
+}
