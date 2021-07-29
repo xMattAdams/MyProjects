b6d4ed321a14f05ae7a5315b9c556d50229244b1
xMattAdams <mat.adamczyk@icloud.com>
2021-07-29 19:59:14 +0200

    ToDoList

---
+++ ToDoListProject/ToDoListWebApi/Models/ToDo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoListWebApi.Models
+{
+    public class ToDo
+    {
+        [Key]
+        public int ToDoId { get; set; }
+
+        public string Content { get; set; }
+
+        public bool isDone { get; set; }
+
+        public bool isOutdated { get; set; }
+
+        public DateTime Deadline { get; set; }
+    }
+}
---
+++ ToDoListProject/ToDoListWebApi/Models/ToDoDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoListWebApi.Models
+{
+    public class ToDoDbContext: DbContext
+    {
+        public ToDoDbContext(DbContextOptions<ToDoDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<ToDo> ToDos { get; set; }
+    }
+}
